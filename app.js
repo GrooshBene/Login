c20c848c33ee2f6aee4d537fd422cba620ce943c
GrooshBene <wltn000129@gmail.com>
2015-09-09 00:04:31 +0900

    Login Form Done

---
+++ app.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var passport = require('passport')
+    , LocalStrategy = require('passport-local').Strategy;
+var flash = require('connect-flash') // session 관련해서 사용됨. 로그인 실패시 session등 클리어하는 기능으로 보임.
+
+
+//serializer와 deseriazlier는 필수로 구현해야 함.
+
+// 인증 후, 사용자 정보를 Session에 저장함
+passport.serializeUser(function(user, done) {
+    console.log('serialize');
+    done(null, user);
+});
+
+// 인증 후, 페이지 접근시 마다 사용자 정보를 Session에서 읽어옴.
+passport.deserializeUser(function(user, done) {
+    //findById(id, function (err, user) {
+    console.log('deserialize');
+    console.log(user);
+    done(null, user);
+    //});
+});
+
+
+passport.use(new LocalStrategy({
+        usernameField : 'userid',
+        passwordField : 'password',
+        passReqToCallback : true
+    }
+    ,function(req,userid, password, done) {
+        if(userid=='hello' && password=='world'){
+            var user = { 'userid':'hello',
+                          'email':'hello@world.com'};
+            return done(null,user);
+        }else{
+            return done(null,false);
+        }
+    }
+));
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.post('/login',
+    passport.authenticate('local', { failureRedirect: '/login_fail', failureFlash: true }),
+    function(req, res) {
+        res.redirect('/login_success');
+    });
+
+app.get('/login_success', ensureAuthenticated, function(req, res){
+    res.send(req.user);
+   // res.render('users', { user: req.user });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+function ensureAuthenticated(req, res, next) {
+    // 로그인이 되어 있으면, 다음 파이프라인으로 진행
+    if (req.isAuthenticated()) { return next(); }
+    // 로그인이 안되어 있으면, login 페이지로 진행
+    res.redirect('/login.html');
+}
